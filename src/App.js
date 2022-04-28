514bf6003da2e74a63a8387e557e96f4b2106c7e
Patrick Hagège <hagege.patrick@gmail.com>
2022-04-28 19:57:56 +0200

    iteration 4 - Bonus - Fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 // src/App.js
 import "./App.css";
+import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import Navbar from './components/NavBar';
 import CountriesList from "./components/CountriesList";
 import CountryDetails from "./components/CountryDetails";
-import countries from './countries.json';
+import axios from 'axios';
 
 function App() {
+  const [APICountries, setAPICountries] = useState([]);
+
+  useEffect(()=> {
+    axios
+    .get('https://ih-countries-api.herokuapp.com/countries')
+    .then(response => {
+      setAPICountries(response.data)
+      // console.table('APICountries', APICountries);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    // eslint-disable-next-line
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
     
       <div className="container">
         <div className="row">
-          <CountriesList countries={countries} />
+          <CountriesList countries={APICountries} />
           <Routes>
-          <Route path="/:alpha3Code" element={<CountryDetails countries={countries} />} />
+          <Route path="/:alpha3Code" element={<CountryDetails countries={APICountries} />} />
           </Routes>
         </div>
       </div>
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,58 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Country = ({ countries }) => {
-  const [borders, setBorders] = useState([]);
+  const [APIBorders, setAPIBorders] = useState([]);
+  const [APICountry, setAPICountry] = useState([]);
 
   const { alpha3Code } = useParams();
 
-  const foundCountry = countries.find(
+  const foundAPICountry = countries.find(
     (country) => country.alpha3Code === alpha3Code
   );
 
-  console.log("Found Country", foundCountry);
-
-  const foundBorders = foundCountry.borders.map(borderCode => {
-    const foundBorder = countries.find(
-      country => country.alpha3Code === borderCode)
-      return foundBorder.name.official
+  const foundAPIBorders = foundAPICountry.borders.map(borderCode => {
+    const foundAPIBorder = countries.find(
+      APICountry => APICountry.alpha3Code === borderCode)
+      return foundAPIBorder.name.official
   })
 
-  console.log('FoundBorders', foundBorders);
+  useEffect(() => {
+    axios
+    .get(`https://ih-countries-api.herokuapp.com/countries/${alpha3Code}`)
+    .then(response => {
+      setAPICountry(response.data);
+    })
+    .catch(error => console.log(error))
+  },[alpha3Code])
 
   useEffect(()=> {
-    setBorders(foundBorders);
+    setAPIBorders(foundAPIBorders);
     // eslint-disable-next-line 
   },[alpha3Code])
 
   return (
-    <div className="col-7 Country" key={foundCountry.alpha3Code}>
-      <img src={`https://flagcdn.com/${foundCountry.alpha2Code.toLowerCase()}.svg`} alt="country flag" style={{ width: '300px' }} />
-      <h1>{foundCountry.name.official}</h1>
+    <div className="col-7 Country" key={foundAPICountry.alpha3Code}>
+      <img src={`https://flagcdn.com/${foundAPICountry.alpha2Code.toLowerCase()}.svg`} alt="country flag" style={{ width: '300px' }} />
+      <h1>{foundAPICountry.name.official}</h1>
       <table className="table">
         <thead></thead>
         <tbody>
           <tr>
             <td style={{ width: '30%' }}>Capital</td>
-            <td>{foundCountry.capital[0]}</td>
+            <td>{foundAPICountry.capital[0]}</td>
           </tr>
           <tr>
             <td>Area</td>
             <td>
-              {`${foundCountry.area} km`} <sup>2</sup>
+              {`${foundAPICountry.area} km`} <sup>2</sup>
             </td>
           </tr>
-          {borders.length > 0 && (
+          {APIBorders.length > 0 && (
             <tr>
             <td>Borders</td>
             <td>
               <ul>
-                {borders.map((border, index) => {
+                {APIBorders.map((APIBorder, index) => {
                   return (
-                    <li key={border}>
-                      <Link to={`/${foundCountry.borders[index]}`}>
-                        {border}
+                    <li key={APIBorder}>
+                      <Link to={`/${foundAPICountry.borders[index]}`}>
+                        {APIBorder}
                       </Link>
                     </li>)
                 })}
@@ -61,7 +68,7 @@ const Country = ({ countries }) => {
           </tr>
           )}
         </tbody>
-        <tfoot>{borders.length === 0 && (<tr><td colSpan={2}>{`${foundCountry.name.common} has no border...`}</td></tr>)}</tfoot>
+        <tfoot>{APIBorders.length === 0 && (<tr><td colSpan={2}>{`${foundAPICountry.name.common} has no border...`}</td></tr>)}</tfoot>
       </table>
     </div>
   );
